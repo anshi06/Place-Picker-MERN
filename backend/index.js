37c9181eba0d8f63d43518a8c8d4217d3c210d9b
anshi06 <103870511+anshi06@users.noreply.github.com>
2024-04-20 19:45:51 +0530

    RIght content

--- backend/index.js
+++ backend/index.js
@@ -1,73 +1,73 @@
 
-// const fs = require("fs");
-// const path = require("path");
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const mongoose = require("mongoose");
-
-// const placesRoutes = require("../routes/places-routes");
-// const usersRoutes = require("../routes/users-routes");
-// const HttpError = require("../models/http-error");
-
-// const app = express();
-
-// app.use(bodyParser.json());
-
-// app.use("/uploads/images", express.static(path.join("uploads", "images")));
-
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-
-//   next();
-// });
-
-// app.use("/api/places", placesRoutes);
-// app.use("/api/users", usersRoutes);
-
-// app.use((req, res, next) => {
-//   const error = new HttpError("Could not find this route.", 404);
-//   throw error;
-// });
-
-// app.use((error, req, res, next) => {
-//   if (req.file) {
-//     fs.unlink(req.file.path, (err) => {
-//       console.log(err);
-//     });
-//   }
-//   if (res.headerSent) {
-//     return next(error);
-//   }
-//   res.status(error.code || 500);
-//   res.json({ message: error.message || "An unknown error occurred!" });
-// });
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://anshiii1503:i4f76loK7V52nTkI@cluster0.qbnm2vj.mongodb.net/Place-Picker?retryWrites=true&w=majority&appName=Cluster0"
-//   )
-//   .then(() => {
-//     app.listen(5000);
-//     console.log("Server started!");
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   });
+const fs = require("fs");
+const path = require("path");
 
 const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
+const HttpError = require("./models/http-error");
+
 const app = express();
-const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(bodyParser.json());
+
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route.", 404);
+  throw error;
 });
+
+app.use((error, req, res, next) => {
+  if (req.file) {
+    fs.unlink(req.file.path, (err) => {
+      console.log(err);
+    });
+  }
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://anshiii1503:i4f76loK7V52nTkI@cluster0.qbnm2vj.mongodb.net/Place-Picker?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(5000);
+    console.log("Server started!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// const express = require("express");
+// const app = express();
+// const port = 3000;
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}`);
+// });
