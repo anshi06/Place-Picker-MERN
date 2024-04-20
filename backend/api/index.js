7e733e293efa9435510807f2e9930bd0074bf81d
anshi06 <103870511+anshi06@users.noreply.github.com>
2024-04-20 19:10:04 +0530

    Update index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -5,9 +5,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
-const placesRoutes = require("./routes/places-routes");
-const usersRoutes = require("./routes/users-routes");
-const HttpError = require("./models/http-error");
+const placesRoutes = require("../routes/places-routes");
+const usersRoutes = require("../routes/users-routes");
+const HttpError = require("../models/http-error");
 
 const app = express();
 
