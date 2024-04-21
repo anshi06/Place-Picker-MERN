40a0a475e4c708a4fd3064092cec47800b773d91
anshi06 <aradhya6768@gmail.com>
2024-04-21 21:02:47 +0530

    Error handling

--- frontend/src/shared/components/UIElements/Avatar.js
+++ frontend/src/shared/components/UIElements/Avatar.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React from "react";
+import defaultImg from "../imgs/defaultImg.jpg"
 
-import './Avatar.css';
+import "./Avatar.css";
 
-const Avatar = props => {
+const Avatar = (props) => {
+  const handleError = (event) => {
+    event.target.src = defaultImg
+  };
+
   return (
     <div className={`avatar ${props.className}`} style={props.style}>
       <img
         src={props.image}
         alt={props.alt}
         style={{ width: props.width, height: props.width }}
+        onError={handleError}
       />
     </div>
   );
